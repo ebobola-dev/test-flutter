bc10cf319014c145919abf9fc7b5b2673d5d5a98
ebobola-dev <romawot94@gmail.com>
2024-06-01 15:33:23 +0300

    news_view_screen done

--- lib/screens/news_view/news_view_screen.dart
+++ lib/screens/news_view/news_view_screen.dart
@@ -1,8 +1,11 @@
+import 'dart:developer';
+
+import 'package:era_developers_test_flutter/bloc/news/news_bloc.dart';
 import 'package:era_developers_test_flutter/common_widgets/back_button.dart';
 import 'package:era_developers_test_flutter/repositories/news/models/article.dart';
 import 'package:era_developers_test_flutter/utils/date_extension.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class NewsViewScreen extends StatefulWidget {
   final Article article;
@@ -13,18 +16,59 @@ class NewsViewScreen extends StatefulWidget {
 }
 
 class _NewsViewScreenState extends State<NewsViewScreen> {
-  late final _scrollConrtoller = ScrollController();
+  late final _sliverScrollController = ScrollController();
+
+  @override
+  void initState() {
+    if (!widget.article.readed) {
+      final newsBloc = context.read<NewsBloc>();
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        //? Весь контент помещается на странице, нечего скроллить
+        if (_sliverScrollController.position.maxScrollExtent == 0.0) {
+          log(
+            'Nothing to scroll',
+            name: 'NewsViewScreen',
+          );
+          newsBloc.add(NewsEvent.markOneReaded(articleId: widget.article.id));
+        }
+      });
+
+      //? Если долистали до конца
+      _sliverScrollController.addListener(() {
+        if (_sliverScrollController.position.atEdge) {
+          if (_sliverScrollController.position.pixels != 0) {
+            log(
+              'Scrolled to end',
+              name: 'NewsViewScreen',
+            );
+            newsBloc.add(NewsEvent.markOneReaded(articleId: widget.article.id));
+          }
+        }
+      });
+    }
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _sliverScrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SizedBox(
         width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              child: ClipRRect(
+        child: CustomScrollView(
+          controller: _sliverScrollController,
+          slivers: [
+            SliverAppBar(
+              pinned: true,
+              expandedHeight: MediaQuery.of(context).size.height * .5,
+              collapsedHeight: 110.0,
+              automaticallyImplyLeading: false,
+              flexibleSpace: ClipRRect(
                 borderRadius: BorderRadius.circular(12.0),
                 child: Stack(
                   children: [
@@ -39,27 +83,39 @@ class _NewsViewScreenState extends State<NewsViewScreen> {
                         color: Color(0xBF000000), // BF - 75% opacity
                       ),
                     ),
-                    SafeArea(
-                      child: Align(
-                        alignment: Alignment.topLeft,
-                        child: MyBackButton(
-                          iconColor: Colors.white,
-                          onTap: () => Navigator.pop(context),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 48.0),
-                      child: Align(
-                        alignment: const Alignment(0.0, 0.75),
-                        child: Text(
-                          widget.article.title,
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 2,
-                          style: Theme.of(context)
-                              .textTheme
-                              .titleLarge!
-                              .copyWith(color: Colors.white),
+                    Positioned.fill(
+                      child: SafeArea(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: MyBackButton(
+                                iconColor: Colors.white,
+                                onTap: () => Navigator.pop(context),
+                              ),
+                            ),
+                            Expanded(
+                              child: Align(
+                                alignment: Alignment.bottomCenter,
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                    bottom: 40.0,
+                                    right: 48.0,
+                                  ),
+                                  child: Text(
+                                    widget.article.title,
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 2,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .titleLarge!
+                                        .copyWith(color: Colors.white),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ),
@@ -67,12 +123,13 @@ class _NewsViewScreenState extends State<NewsViewScreen> {
                 ),
               ),
             ),
-            const SizedBox(height: 20.0),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 21.0),
-                child: SingleChildScrollView(
-                  controller: _scrollConrtoller,
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) => Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 21.0,
+                    vertical: 20.0,
+                  ),
                   child: Column(
                     children: [
                       if (widget.article.description != null)
@@ -91,15 +148,16 @@ class _NewsViewScreenState extends State<NewsViewScreen> {
                         child: Text(
                           widget
                               .article.publicationDate.fullHumanReadableString,
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyMedium!
-                              .copyWith(color: Theme.of(context).dividerColor),
+                          style:
+                              Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                    color: Theme.of(context).dividerColor,
+                                  ),
                         ),
                       ),
                     ],
                   ),
                 ),
+                childCount: 1,
               ),
             ),
           ],
